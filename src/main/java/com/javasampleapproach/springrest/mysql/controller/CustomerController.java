c3f0b63f8426828fb1e563537a36eeb9fecdfe45
softeam2 <softeam2@FRP02594.softeam.com>
2020-01-05 23:19:26 +0100

    modify angular json

--- src/main/java/com/javasampleapproach/springrest/mysql/controller/CustomerController.java
+++ src/main/java/com/javasampleapproach/springrest/mysql/controller/CustomerController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class CustomerController {
