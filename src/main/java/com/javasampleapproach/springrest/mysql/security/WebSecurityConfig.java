5f2a70170f3433120434abccbc72e5b24c8e7b82
softeam2 <softeam2@FRP02594.softeam.com>
2020-01-05 22:46:36 +0100

    disabled security

--- src/main/java/com/javasampleapproach/springrest/mysql/security/WebSecurityConfig.java
+++ src/main/java/com/javasampleapproach/springrest/mysql/security/WebSecurityConfig.java
@@ -54,16 +54,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().
-                authorizeRequests()
-              .antMatchers("*/*")
-                //.antMatchers("/api/auth/**", "/api/**","/report/**")
-                .permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//        http.cors().and().csrf().disable().
+//                authorizeRequests()
+//                .antMatchers("/api/auth/**", "/api/**","/report/**")
+//                .permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        
+//        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
